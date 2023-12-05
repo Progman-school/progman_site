b4fa9333e51e097437e9ae55991f5da206721e82
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-12-04 21:52:48 -0500

    tg hook fix1

--- routes/tg_api.php
+++ routes/tg_api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\TelegramApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('set_hook', [TelegramApiController::class, 'setHook']);
+Route::get('/', function () {return "ok";});
