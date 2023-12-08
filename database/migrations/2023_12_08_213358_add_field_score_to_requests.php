843a1a4382a2521ab575871b51cbcbfe896e44c1
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-12-08 17:32:03 -0500

    migration rename_requests_to_user_request fix2

--- database/migrations/2023_12_08_213358_add_field_score_to_requests.php
+++ database/migrations/2023_12_08_213358_add_field_score_to_requests.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,11 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::beginTransaction();
         Schema::table('requests', function(Blueprint $table) {
             $table->integer('test_score')->nullable(false);
             $table->foreignId("request_id")->nullable(false)
                 ->constrained()->cascadeOnDelete();
         });
+        DB::commit();
     }
 
     /**
@@ -23,9 +26,11 @@ return new class extends Migration
      */
     public function down(): void
     {
+        DB::beginTransaction();
         Schema::table('requests', function (Blueprint $table) {
             $table->dropColumn('test_score');
             $table->dropConstrainedForeignId("course_id");
         });
+        DB::commit();
     }
 };
