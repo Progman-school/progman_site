ac1d026e116ebcef90c57945c5d32098f2a34055
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-12-08 18:53:51 -0500

    added courese and uid-type to the front-end

--- resources/js/components/pages/test.vue
+++ resources/js/components/pages/test.vue
@@ -35,7 +35,7 @@ const saveTestData = (form) => {
     form.preventDefault()
     const formData = new FormData(form.target)
     mixins.methods.postAPI(
-        'savetest',
+        'add_request',
         formData,
         showBotLogin
     )
@@ -52,7 +52,21 @@ const saveTestData = (form) => {
         <section>
             <h3 class="major">Answer the question:</h3>
             <form id="test_form" @submit="saveTestData">
+                <label for="course">
+                    Choose the course that you are interesting in:
+                </label>
+                <select id="course" name="course_id">
+                    <option value="1" selected>WEB full-stack</option>
+                    <option value="2" selected>Different..</option>
+                </select>
                 <insert-content set_class="fields">test_for_registration</insert-content>
+                <label for="uid_type">
+                    Select where would you like to receive your test results and advices:
+                </label>
+                <select id="uid_type" name="uid_type">
+                    <option value="telegram" selected>Telegram</option>
+                    <option value="email" selected>E-Mail</option>
+                </select>
                 <ul class="actions">
                     <li><button type="submit" class="primary">Finish the test</button></li>
                 </ul>
