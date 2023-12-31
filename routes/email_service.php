fa0e80514f5b91948e48f9f1753fdade56239160
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-12-31 01:22:43 -0500

    start email login

---
+++ routes/email_service.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\TelegramApiController;
+use App\sdks\TelegramBotApiSdk;
+use Illuminate\Support\Facades\Route;
+
+Route::post(TelegramBotApiSdk::API_ENTRYPOINT, [TelegramApiController::class, 'entry']);
+Route::get("/", [TelegramApiController::class, 'setHook']);
