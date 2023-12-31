4d214cd9463f2632bec0958779672884b5ebb61c
Denis progman <95615037+denis-progman@users.noreply.github.com>
2023-12-31 17:22:36 -0500

    not tested email login

---
+++ resources/views/api_answers/result_page_of_email_action.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Result Status | ProgMan.site</title>
+</head>
+<body>
+    <div style="text-align: center">
+        <h3>Status:</h3>
+        <p><b>{{$text}}</b></p>
+        <a href="/" title="progman.site">Go back to the site >></a>
+    </div>
+</body>
+</html>
