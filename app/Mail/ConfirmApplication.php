7dbb903aaa2bec8689ba5ed0f2bae03cd51852c9
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-04 18:28:30 -0500

    remade mail confirmation with laravel stuff

---
+++ app/Mail/ConfirmApplication.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ConfirmApplication extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    protected string $confirmUrl;
+
+    protected int $score;
+
+    protected string $text;
+
+    public function __construct(string $confirmUrl, int $score, string $text)
+    {
+        $this->confirmUrl = $confirmUrl;
+        $this->text = $text;
+        $this->score = $score;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Confirm Application',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mails.application_confirmation',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
