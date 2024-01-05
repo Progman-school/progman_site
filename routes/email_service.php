efce418b27f2958e834aa5834f0c70bcf03aa554
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-04 22:09:01 -0500

    check new version of mail service

--- routes/email_service.php
+++ routes/email_service.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\MailController;
 use App\sdks\EmailServiceSdk;
 use Illuminate\Support\Facades\Route;
 
 Route::post(EmailServiceSdk::API_ENTRYPOINT, [EmailServiceSdk::class, 'entry']);
+Route::get("test", [MailController::class, 'sendTestMessage']);
