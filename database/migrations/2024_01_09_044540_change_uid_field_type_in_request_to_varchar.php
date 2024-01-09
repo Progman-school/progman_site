4dcf3d0247a605ec32c4f2adf73a6a8509d60d41
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-08 23:52:43 -0500

    migration change_uid_field_type_in_request_to_varchar fix

--- database/migrations/2024_01_09_044540_change_uid_field_type_in_request_to_varchar.php
+++ database/migrations/2024_01_09_044540_change_uid_field_type_in_request_to_varchar.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('requests', function (Blueprint $table) {
-            $table->string('uid')->change();
+            $table->string('uid')->nullable()->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('requests', function (Blueprint $table) {
-            $table->bigInteger('uid')->change();
+            $table->bigInteger('uid')->nullable()->change();
         });
     }
 };
