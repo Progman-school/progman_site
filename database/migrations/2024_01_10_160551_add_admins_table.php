1d7489eb03dc3bbff1ee5118909b4ec00caab4fd
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-10 11:21:43 -0500

    migration for admin the panel add_admins_table fix

--- database/migrations/2024_01_10_160551_add_admins_table.php
+++ database/migrations/2024_01_10_160551_add_admins_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('admins', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('login');
             $table->string('password');
@@ -20,7 +20,7 @@ return new class extends Migration
         });
     }
 
-    /**
+   /**
      * Reverse the migrations.
      */
     public function down(): void
