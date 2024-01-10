98f795ece1da47cebdac7f0cf06353ea079c5a4a
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-10 11:14:33 -0500

    migration for admin the panel add_admins_table

---
+++ database/migrations/2024_01_10_160551_add_admins_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('login');
+            $table->string('password');
+            $table->enum('access', ['moderator', 'editor', 'admin']);
+            $table->boolean('is_active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
