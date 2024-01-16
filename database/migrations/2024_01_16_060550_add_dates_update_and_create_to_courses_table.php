51c6cedaa9a01f37601e11b7763b490e2f69a2f0
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-16 01:08:10 -0500

    migration: add_dates_update_and_create_to_courses_table

---
+++ database/migrations/2024_01_16_060550_add_dates_update_and_create_to_courses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->timestamp('created_at')->useCurrent();
+        });
+        Schema::table('courses', function (Blueprint $table) {
+            $table->timestamp('updated_at')->nullable()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+    }
+};
