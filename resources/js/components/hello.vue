375321742d483c76365842efd039335e08a14cc7
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-23 14:22:10 -0500

    feature: course corousel wiget for the main page (try)

--- resources/js/components/hello.vue
+++ resources/js/components/hello.vue
@@ -8,6 +8,16 @@
                 <p>
                     <insert-content>main_page_text</insert-content>
                 </p>
+                <div>
+                        <carousel-3d>
+                            <slide :index="0">
+                                <span>Slide 1 Content</span>
+                            </slide>
+                            <slide :index="1">
+                                <span>Slide 2 Content</span>
+                            </slide>
+                        </carousel-3d>
+                </div>
             </div>
         </div>
         <navigate></navigate>
@@ -17,11 +27,17 @@
 <script>
     import insertContent from '../components/helpers/insert-content.vue'
     import navigate from "./navigate.vue"
+    // import courseCarousel from "../components/helpers/course_carousel.vue"
+    import { Carousel3d, Slide } from 'vue-carousel-3d';
+
     export default {
         name: "hello",
         components: {
             insertContent,
-            navigate
+            navigate,
+            Carousel3d,
+            Slide
+            // courseCarousel,
         },
     }
 </script>
