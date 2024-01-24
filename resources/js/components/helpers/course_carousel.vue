afc8c65de95ffbd71cc3aa017b79f65dc5f39d6b
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-01-24 18:38:11 -0500

    feature: details and tecchnologies for course corousel wiget

--- resources/js/components/helpers/course_carousel.vue
+++ resources/js/components/helpers/course_carousel.vue
@@ -4,6 +4,7 @@ import {useMultiLanguageStore} from "../../storages/multi_language_content";
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 import {ref} from "vue";
+import router from '../../router.js'
 
 const multiLanguageStore = useMultiLanguageStore()
 const preloadedDataStorage = usePreloadedDataStorage()
@@ -11,15 +12,27 @@ const preloadedDataStorage = usePreloadedDataStorage()
 const name = "course-carousel"
 preloadedDataStorage.getCoursesList();
 
+const autoPlay = ref(3000)
+
 const toggleDetails = (event) => {
-    console.log(event.target.parentElement)
-    event.target.parentElement.querySelector(".short_details p").style.display = event.target.p.style.display === "none" ? "block" : "none"
-    event.target.parentElement.querySelector(".full_details div").style.display = event.target.div.style.display === "none" ? "block" : "none"
+    autoPlay.value = 0
+    if (!event.target.parentElement.parentElement.classList.contains("carousel__slide--active")) {
+        return false
+    }
+    event.target.parentElement.parentElement.parentElement.querySelectorAll(".short_details, .full_details").forEach((element) => {
+        element.style.display = element.style.display === "none" ? "block" : "none"
+        element.style.display = element.style.display === "none" ? "block" : "none"
+    })
+    const shortDetails = event.target.parentElement.querySelector(".short_details")
+    const fullDetails = event.target.parentElement.querySelector(".full_details")
+    shortDetails.style.display = shortDetails.style.display === "none" ? "block" : "none"
+    fullDetails.style.display = fullDetails.style.display === "none" ? "block" : "none"
 }
+
 </script>
 
 <template>
-    <Carousel :itemsToShow="3.95" :wrapAround="true" :transition="500">
+    <Carousel :autoplay=autoPlay :itemsToShow="3.32" :wrapAround="true" :transition="600">
         <Slide v-for="course in preloadedDataStorage.courses" :key="course.id">
             <div class="carousel__item">
                 <h5 @click="toggleDetails">{{course.name}}</h5>
@@ -31,6 +44,7 @@ const toggleDetails = (event) => {
                             {{technology.name}}
                         </li>
                     </ul>
+                    <a @click="router.push('/test')">>  start  &lt;</a>
                 </div>
             </div>
         </Slide>
@@ -40,8 +54,8 @@ const toggleDetails = (event) => {
 <style scoped>
 .carousel__item {
     background: rgba(1, 3, 4, 0.4);
-    border-right: white 1px solid;
-    border-left: white 1px solid;
+    border-right: black 1px solid;
+    border-left: black 1px solid;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
     color: #000;
     text-align: center;
@@ -53,17 +67,32 @@ const toggleDetails = (event) => {
     font-size: 8px !important;
     cursor: default;
     margin: 0;
+    letter-spacing: 0 !important;
+    text-transform: none !important;
 }
 
-.carousel__item div > p {
+.full_details {
     overflow-y: scroll;
-    height: 100px !important;
+    height: 120px !important;
 }
 .carousel__item h5 {
-    padding-top: 15px;
+    margin: 5px 0 !important;
+    padding: 0 2px !important;
     cursor: default;
+    letter-spacing: 0 !important;
 }
-.carousel__item h5:hover {
+
+.carousel__item a {
+    text-transform: capitalize;
+    font-size: 12px;
+    margin-bottom: 10px;
+    color: #50aa60;
+}
+.carousel__item a:hover {
+    text-decoration: underline;
+    cursor: pointer;
+}
+.carousel__slide--active .carousel__item h5:hover {
     text-decoration: underline;
     cursor: pointer;
     color: #50aa60;
@@ -74,17 +103,24 @@ const toggleDetails = (event) => {
     grid-template-columns: repeat(3,1fr);
     grid-gap: 2px;
     padding: 0 6px;
+    margin: 0;
 }
 .carousel__item ul li {
     color: white;
     font-size: 6px;
-    padding: 2px;
-    border-bottom: 1px solid lightgrey;
+    padding: 1px;
     cursor: default;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.carousel__item ul li:hover {
+    color: #35c44e;
+    text-decoration: underline;
 }
 
 .carousel__slide {
-    padding: 5px;
+    padding: 20px;
 }
 
 .carousel__viewport {
@@ -100,7 +136,7 @@ const toggleDetails = (event) => {
 }
 
 .carousel__slide {
-    opacity: 0.9;
+    opacity: 0.8;
     transform: rotateY(-20deg) scale(0.9);
 }
 
@@ -120,6 +156,6 @@ const toggleDetails = (event) => {
 
 .carousel__slide--active {
     opacity: 1;
-    transform: rotateY(0) scale(1.1);
+    transform: rotateY(0) scale(1.3);
 }
 </style>
