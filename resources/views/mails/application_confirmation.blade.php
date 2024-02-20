6a9eaac428cb0694f4cc218b1a852d296f2586a2
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-02-19 21:41:09 -0500

    feature: more green text style (fix)

--- resources/views/mails/application_confirmation.blade.php
+++ resources/views/mails/application_confirmation.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
     <div style="text-align: center">
-        <h3>Your score: {{$score}}%</h3>
+        <h3><span class="important_text">Test score: {{$score}}%</span></h3>
         <p><b>{!! $text !!}</b></p>
     </div>
     @component('mail::button', ['url' => $confirmUrl])
