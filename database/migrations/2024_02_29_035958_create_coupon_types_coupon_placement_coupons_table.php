6f020d4b1193a9beba7a8f6addc13a4c1d127c0c
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-02-29 02:06:34 -0500

    feature: coupon objects stuff (fix 5)

--- database/migrations/2024_02_29_035958_create_coupon_types_coupon_placement_coupons_table.php
+++ database/migrations/2024_02_29_035958_create_coupon_types_coupon_placement_coupons_table.php
@@ -14,55 +14,47 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::beginTransaction();
+        Schema::create('coupon_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->unique();
+            $table->string('prefix', 16)->unique();
+            $table->string('use_link', 128);
+            $table->string('description_' . \App\Services\TagService::EN_LANGUAGE);
+            $table->string('description_' . \App\Services\TagService::RU_LANGUAGE);
+        });
 
-        try {
-            Schema::create('coupon_types', function (Blueprint $table) {
-                $table->id();
-                $table->string('name', 128)->unique();
-                $table->string('prefix', 16)->unique();
-                $table->string('use_link', 128);
-                $table->string('description_' . \App\Services\TagService::EN_LANGUAGE);
-                $table->string('description_' . \App\Services\TagService::RU_LANGUAGE);
-            });
+        Schema::create('coupon_units', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->unique();
+            $table->string('symbol', 128)->unique();
+            $table->enum('symbol_placement', Coupon\CouponUnit::SYMBOL_PLACEMENTS)->default('after');
+            $table->string('formula', 512);
+        });
 
-            Schema::create('coupon_units', function (Blueprint $table) {
-                $table->id();
-                $table->string('name', 128)->unique();
-                $table->string('symbol', 128)->unique();
-                $table->enum('symbol_placement', Coupon\CouponUnit::SYMBOL_PLACEMENTS)->default('after');
-                $table->string('formula', 512);
-            });
+        Schema::create('coupon_placements', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->unique();
+        });
 
-            Schema::create('coupon_placements', function (Blueprint $table) {
-                $table->id();
-                $table->string('name', 128)->unique();
-            });
-
-            Schema::create('coupons', function (Blueprint $table) {
-                $table->id();
-                $table->string('name', 128);
-                $table->string('serial_number', 64)->unique();
-                $table->string('description')->nullable();
-                $table->enum('language', \App\Services\TagService::LANG_LIST)->nullable(false);
-                $table->enum('method', Coupon::METHODS);
-                $table->foreignId('coupon_type_id')->constrained('coupon_types');
-                $table->timestamp('created_at')->useCurrent();
-                $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable(true)->default(null);
-                $table->timestamp('expired_at');
-                $table->integer('value');
-                $table->foreignId('coupon_unit_id')->constrained('coupon_units');
-                $table->integer('max_times')->default(1);
-                $table->integer('used_times')->default(0)->nullable(false);
-                $table->enum('area_type', ['online', 'offline']);
-                $table->string('area', 512);
-                $table->foreignId('placement_id')->constrained('coupon_placements');
-            });
-            DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+            $table->string('serial_number', 64)->unique();
+            $table->string('description')->nullable();
+            $table->enum('language', \App\Services\TagService::LANG_LIST)->nullable(false);
+            $table->enum('method', Coupon::METHODS);
+            $table->foreignId('coupon_type_id')->constrained('coupon_types');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrentOnUpdate()->nullable()->default(null);
+            $table->timestamp('expired_at')->nullable()->default(null);
+            $table->integer('value');
+            $table->foreignId('coupon_unit_id')->constrained('coupon_units');
+            $table->integer('max_times')->default(1);
+            $table->integer('used_times')->default(0)->nullable(false);
+            $table->enum('area_type', ['online', 'offline']);
+            $table->string('area', 512);
+            $table->foreignId('placement_id')->constrained('coupon_placements');
+        });
     }
 
     /**
