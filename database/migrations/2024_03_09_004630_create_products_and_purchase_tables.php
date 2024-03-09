c6508d4b9fd696102100f5557386238ca055178f
Denis progman <95615037+denis-progman@users.noreply.github.com>
2024-03-08 20:16:25 -0500

    feature: Products and Purchase models + migration: create_products_and_purchase_tables (fix)

--- database/migrations/2024_03_09_004630_create_products_and_purchase_tables.php
+++ database/migrations/2024_03_09_004630_create_products_and_purchase_tables.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Models\Coupon;
+use App\Models\Coupon\CouponType;
 use App\Models\Course;
+use App\Models\Product;
+use App\Models\Request as UserRequest;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,19 +25,21 @@ return new class extends Migration
             $table->text('description_ru');
             $table->float('price');
             $table->string('image_url')->nullable()->default(null);
+            $table->foreignIdFor(CouponType::class)->nullable()->default(null)->constrained();
             $table->foreignIdFor(Course::class)->nullable()->default(null)->constrained();
         });
 
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
             $table->timestamp('created_at')->useCurrent();
-            $table->foreignId('request_id')->nullable()->default(null)->constrained();
-            $table->foreignId('product_id')->nullable(false)->constrained();
+            $table->foreignIdFor(UserRequest::class)->nullable()->default(null)->constrained();
+            $table->foreignIdFor(Product::class)->nullable(false)->constrained();
+            $table->foreignIdFor(Coupon::class)->nullable()->default(null)->constrained();
             $table->integer('quantity');
             $table->float('current_product_price');
             $table->float('total_price');
             $table->string('contact');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignIdFor(User::class)->constrained();
             $table->enum('payment_type', \App\Models\Purchase::PAYMENT_TYPES);
             $table->json('payment_details');
             $table->text('comment')->nullable()->default(null);
